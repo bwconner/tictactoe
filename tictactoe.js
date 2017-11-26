c6627c0d1898c873a93b7b048b38210026879a16
Bruce Conner <bruce.conner.ii@gmail.com>
2017-11-26 11:18:41 -0500

    Javscript Updates
    More shelled out javascript functions for computer and player
    interactions in tic tac toe.

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,7 @@
 var playerTurn = true;
-//var currentBoard[[0,0,0][0,0,0][0,0,0]];
+var currentBoard = [[2,0,2],
+					[0,0,0],
+					[2,0,2]];
 
 function beginGame () {
 	var playerStart = determineOrder();
@@ -10,7 +12,7 @@ function beginGame () {
 	} else {
 		playerTurn = false;
 		$(".turn-information").text("Computer's Turn");
-		computerTurn();
+		beginComputerTurn();
 	}
 
 	playerClickHandler();
@@ -21,34 +23,53 @@ function determineOrder() {
 }
 
 function playerClickHandler() {
-	if (!playerTurn) {
-		return;
-	}
-
 	$(".game-cell").on("click", function() {
+		if (playerTurn === false) {
+			return;
+		}
+
 		var $playerChoice = $(this);
 		if ($playerChoice.hasClass("played")) {
 			//invalid choice error
 		} else {
 			$playerChoice.addClass("played");
 			$playerChoice.addClass("player-cell");
+			endPlayerTurn();
 		}
 	});
+}
+
+function beginComputerTurn() {
+	alert(Math.max(...currentBoard[0]));
+	alert(Math.max(...currentBoard[1]));
+	alert(Math.max(...currentBoard[2]));
+
+	if ((".played").length <= 0) {
+
+	}
+}
+
+function endComputerTurn() {
+	playerTurn = true;
+	$(".turn-information").text("Player's Turn");
+}
+
+function beginPlayerTurn() {
 
 }
 
-function computerTurn() {
-
+function endPlayerTurn() {
+	playerTurn = false;
+	$(".turn-information").text("Computer's Turn");
 }
 
-function endTurn () {
-
+function checkForWin() {
+	
 }
-
+ 
 function resetBoard() {
 
 }
-
 
 $(document).ready(function() {
 	beginGame();
